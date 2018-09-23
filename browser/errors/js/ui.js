32b58c172155f900a2b2e8173a41bdf02f66ad2e
Konstantin Pronin <troyandex@gmail.com>
2018-09-23 18:37:41 +0400

    HomeWork 1 - add Errors

--- browser/errors/js/ui.js
+++ browser/errors/js/ui.js
@@ -1,6 +1,6 @@
 'use strict';
-function handleClick {
-  openMail();
+
+const button = document.getElementById('open');
+button.onclick = function handleClick() {
+  openMail(); 
 }
-const button = window.getElementById('open');
-button.onclick = handleClick();
