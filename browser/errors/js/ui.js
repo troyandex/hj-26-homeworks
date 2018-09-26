f982b36fac4130dffcaa6bd40b1bef6f88771250
Konstantin Pronin <troyandex@gmail.com>
2018-09-26 10:28:50 +0400

    HomeWork 1 - errors - fix by instructions

--- browser/errors/js/ui.js
+++ browser/errors/js/ui.js
@@ -1,6 +1,13 @@
 'use strict';
+function handleClick() {
+  openMail();
+}
+const button = document.getElementById('open');
+button.onclick = handleClick;
 
+
+/* Мой вариант - работает но не по заданию:
 const button = document.getElementById('open');
 button.onclick = function handleClick() {
   openMail(); 
-}
+} */
