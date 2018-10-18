7550006166bdbeac6f3b67ddeb28c452c537451a
Konstantin Pronin <troyandex@gmail.com>
2018-10-18 13:33:02 +0400

    HomeWork 8 - table - done

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,17 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+	if (event.target.classList.contains('prop__name')) {
+		// console.log(event.target.classList);
+		let sortDirection = 1;
+    let sortField = event.target.dataset.propName;
+    
+    if (event.target.dataset.dir === '1') {
+      sortDirection = -1;
+    }
+    event.target.dataset.dir = sortDirection;
+
+    table.dataset.sortBy = sortField;
+    sortTable(sortField, sortDirection);
+	}
 }
