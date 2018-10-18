eca79652bcb1f0687e5223f99589da39a694b3ac
Konstantin Pronin <troyandex@gmail.com>
2018-10-18 23:20:11 +0400

    HomeWork 7 - todo-list - del comments

--- html-document-structure/todo-list/js/todo.js
+++ html-document-structure/todo-list/js/todo.js
@@ -14,17 +14,9 @@ function updateList() {
 
 	if (label.querySelector('input').checked) {
 		console.log('-true-');
-		// 1 вариант = input.removeAtribute('checked');
-		// 2 вариант = input.checked = false;
 		undoneList.appendChild(label);
 	} else {
 		console.log('-false-');
-		// 1 вариант = input.setAtribute('checked');
-		// 2 вариант = input.checked = true;
 		doneList.appendChild(label);
 	}
 }
-
-// нужно доработать изменения checked (не получается добавить/убрать)
-// или я не правильно понял?
-// по идее работает и так
