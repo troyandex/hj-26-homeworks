bdb4cb7ed16869f875aab6279f44ea2193860fd7
Konstantin Pronin <troyandex@gmail.com>
2018-10-18 14:22:22 +0400

    HomeWork 8 - shop - done

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,21 @@
 'use strict';
+
+document.querySelector('.items-list').addEventListener('click', addToCartButtonFunction);
+
+function addToCartButtonFunction(event) {
+    if (!event.target.classList.contains('add-to-cart')) {
+        return;
+    }
+    event.preventDefault();
+
+    const title = event.target.dataset.title;
+    const price = event.target.dataset.price;
+
+    // находим выбранный товар
+    const item = items // items = массив данных из loader.js
+        .filter(item => ( (item.title === title) && (item.price === +price) ))
+        .pop();
+
+    // функция из loader.js
+    addToCart(item);
+}
