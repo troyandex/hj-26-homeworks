14062f5e39a07f593d7a6545f8a9f6e886f4488e
Konstantin Pronin <troyandex@gmail.com>
2018-10-21 19:26:15 +0400

    HomeWork 9 - counter done

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const counter = document.getElementById('counter');
+const plusBtn = document.getElementById('increment');
+const decrementBtn = document.getElementById('decrement');
+const resetBtn = document.getElementById('reset');
+
+function initCounter () {
+	if (!localStorage.counter) {
+    localStorage.counter = 0;
+	}
+	counter.textContent = localStorage.counter
+}
+
+plusBtn.addEventListener('click', () => {
+  localStorage.counter++;
+  initCounter();
+});
+
+decrementBtn.addEventListener('click', () => {
+  localStorage.counter--;
+  initCounter();
+});
+
+resetBtn.addEventListener('click', () => {
+  localStorage.counter = 0;
+  initCounter();
+});
+
+initCounter();
