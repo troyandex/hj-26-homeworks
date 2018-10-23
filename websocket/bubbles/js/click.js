94d449ae31ebfbdd675c400b6799aebb03d93fa8
Konstantin Pronin <troyandex@gmail.com>
2018-10-23 23:30:13 +0400

    HomeWork 10 - bubbles done

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+showBubbles(connection);
+
+document.querySelector('body').addEventListener('click', event => {
+    const coords = {
+        x: event.clientX,
+        y: event.clientY
+    };
+    // console.log(coords);
+    connection.send(JSON.stringify(coords));
+    showBubbles(connection);
+})
