c58aed03141778480d03d85dd04c6e93cc2530b6
Konstantin Pronin <troyandex@gmail.com>
2018-10-23 23:38:17 +0400

    HomeWork 10 - counter done

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/counter');
+const usersCounter = document.querySelector('.counter');
+const errorsCounter = document.querySelector('output.errors');
+
+window.addEventListener('beforeunload', () => {
+  connection.close(1000, 'Пользователь вышел');
+});
+
+connection.addEventListener('message', (event) => {
+  let data = JSON.parse(event.data);
+
+  usersCounter.innerText = data.connections;
+  errorsCounter.innerText = data.errors
+});
