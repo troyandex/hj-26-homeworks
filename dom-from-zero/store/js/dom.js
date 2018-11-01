1216801f177ffaec3a8f92f92e08e6b0dd0e2453
Konstantin Pronin <troyandex@gmail.com>
2018-11-01 23:57:13 +0400

    HomeWork 13 - store done

--- dom-from-zero/store/js/dom.js
+++ dom-from-zero/store/js/dom.js
@@ -1,1 +1,30 @@
 'use strict';
+function createElement(block) {
+  if ((typeof block === 'string') || (typeof block === 'number') || (block === true)) {
+    return document.createTextNode(block);
+  }
+
+  if ((block === undefined) || (block === null) || (block === false)) {
+    return document.createTextNode('');
+  }
+
+  if (Array.isArray(block)) {
+    return block.reduce((f, elem) => {
+      f.appendChild(createElement(elem));
+
+      return f;
+    }, document.createDocumentFragment());
+  }
+
+  const element = document.createElement(block.name);
+
+  if (block.props) {
+    Object.keys(block.props).forEach(key => {
+      element.setAttribute(key, block.props[key]);
+    });
+  }
+
+  element.appendChild(createElement(block.childs));
+
+  return element;
+}
