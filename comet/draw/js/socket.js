5dfd5f3e3f7768fb220235bd8b375671a7dd2e0f
Konstantin Pronin <troyandex@gmail.com>
2018-11-04 16:39:01 +0400

    HomeWork 14 - draw done

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -1,1 +1,6 @@
 'use strict';
+const ws = new WebSocket('wss://neto-api.herokuapp.com/draw');
+
+window.editor.addEventListener('update', event => {
+  event.canvas.toBlob(blob => ws.send(blob));
+});
